144c3c261f15ddb38505e34ab885ec088338d3f1
priyaprem <priyaprem08@gmail.com>
2021-12-26 23:22:27 +0530

    initial commit

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/employee_management'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db =SQLAlchemy(app)
 
-class Data(db.Model):
+class EmployeeDetails(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    phone = db.Column(db.String(100))
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    phone = db.Column(db.String(255))
+    address = db.Column(db.String(255))
 
-    def __init__(self,name,email,phone):
+    def __init__(self,name,email,phone,address):
         self.name = name
         self.email = email
         self.phone = phone
+        self.address = address
 
 
 @app.route('/')
 def index():
-    all_data = Data.query.all()
+    all_data = EmployeeDetails.query.all()
     return render_template('index.html',employees = all_data)
 
 @app.route('/insert',methods=['POST'])
@@ -31,7 +33,8 @@ def insert():
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
-        my_data = Data(name, email, phone)
+        address = request.form['address']
+        my_data = EmployeeDetails(name, email, phone, address)
         db.session.add(my_data)
         db.session.commit()
 
@@ -42,10 +45,11 @@ def insert():
 @app.route('/update', methods=['GET','POST'])
 def update():
     if request.method == 'POST':
-        my_data = Data.query.get(request.form.get('id'))
+        my_data = EmployeeDetails.query.get(request.form.get('id'))
         my_data.name = request.form['name']
         my_data.email = request.form['email']
         my_data.phone = request.form['phone']
+        my_data.address = request.form['address']
         db.session.commit()
 
         flash("Employee updates successfully")
@@ -54,7 +58,7 @@ def update():
 
 @app.route('/delete/<id>',methods = ['GET','POST'])
 def delete(id):
-    my_data = Data.query.get(id)
+    my_data = EmployeeDetails.query.get(id)
     db.session.delete(my_data)
     db.session.commit()
 
@@ -64,7 +68,7 @@ def delete(id):
 
 @app.route('/delete_all',methods = ['GET','POST'])
 def delete_all():
-    db.session.query(Data).delete()
+    db.session.query(EmployeeDetails).delete()
     db.session.commit()
 
     flash("All Employees Deleted Successfully")
